c87677639f5f29f42653bd60a6a3840c76de7212
Toaster_Machine <jeremy.villanova@orange.fr>
2025-01-07 10:17:49 +0100

    Qualité de code

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
 from src.functions.cleaner import *
 from src.functions.descriptive_statistics import *
 from src.functions.seats_entry_2022 import *
 
 from src.values.cinemas_columns import *
-
-def get_seats_diagram(seat) :
-    seats_df = pd.DataFrame(seat)
-    seats_df.set_index(seat.index)[
-        ["entrée_moyenne"]].plot(kind='bar', figsize=(12, 6))
-    plt.table('Entrées moyenne par fauteuils dans les salles de cinémas')
-    plt.ylabel('entrée_moyenne')
-    plt.xlabel('commune')
-    plt.xticks(rotation=45)
-    plt.grid(axis='y')
-    plt.legend(title="Légende")
-    plt.show()
-
 
 cinemas_file = get_clean_data('data/cinemas.csv') # Nettoyage des données
 
@@ -30,37 +14,6 @@ cinemas_data = cinemas_filt_columns
 cinemas_means = get_means(cinemas_data, cinemas_columns_means)
 cinemas_std = get_std(cinemas_data, cinemas_columns_means)
 
-# cinemas_entry_per_seat = get_entry_per_seats_2022(cinemas_data)
-
-average = cinemas_data["entrées 2022"] / cinemas_data["fauteuils"]
-tab =[]
-for el in average:
-    tab.append(el)
-
-cinemas_data.insert(loc=7, column="entry_seats_2022", value=tab)
-
-# print(cinemas_data.groupby("commune").agg(
-#     entrée_moyenne=("entry_seats_2022", "mean")
-# ))
-
-# print(cinemas_data.groupby("commune").agg(
-#     entrée_moyenne=("entry_seats_2022", "mean")
-# ).sort_values(by='entrée_moyenne', ascending=False).head(3))
-
-# print(cinemas_data.groupby("commune").agg(
-#     entrée_moyenne=("entry_seats_2022", "mean")
-# ).sort_values(by='entrée_moyenne', ascending=True).head(3))
-
-
-cinemas_seats_data = cinemas_data.groupby("commune").agg(
-    entrée_moyenne=("entry_seats_2022", "mean")
-).sort_values(by='entrée_moyenne', ascending=False).head(10)
-
-# print(cinemas_data)
-print(cinemas_seats_data)
-# print(cinemas_seats_data.index)
-
-get_seats_diagram(cinemas_seats_data)
 
 # print(cinemas_data.head())
 # print('\n --- \n')
@@ -68,3 +21,6 @@ get_seats_diagram(cinemas_seats_data)
 # print(cinemas_std)
 # print('\n --- \n')
 # print(cinemas_entry_per_seat)
+# print(get_entry_per_seats_2022(cinemas_data, cinemas_columns_entry_per_seats))
+# print(get_entry_per_seats_2022(cinemas_data, cinemas_columns_entry_per_seats, True))
+# get_seats_diagram(cinemas_data, cinemas_columns_entry_per_seats)
--- src/functions/seats_entry_2022.py
+++ src/functions/seats_entry_2022.py
@@ -1,8 +1,30 @@
-def get_entry_per_seats_2022(df):
-    mean = ''
-    for el in df["commune"]:
-        line = df[df["commune"] == el]
-        df["entrées 2022", "fauteuils"]
-        division = str((line["entrées 2022"] / line["fauteuils"]))
-        mean += "Les entrées moyenne par fauteuil de la region " + el + " est d'environ : " + division + "\n"
-    return mean
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def get_entry_per_seats_2022(df, columns, ascending=False, head=3):
+    average = df[columns[2]] / df[columns[1]]
+    tab =[]
+    for el in average:
+        tab.append(el)
+
+    if "entry_seats_2022" not in df.columns:
+        df.insert(loc=7, column="entry_seats_2022", value=tab)
+
+    entry_per_seat = df.groupby(columns[0]).agg(
+            entrée_moyenne=("entry_seats_2022", "mean")
+        ).sort_values(by='entrée_moyenne', ascending=ascending).head(head)
+
+    return entry_per_seat
+
+def get_seats_diagram(df, columns) :
+    df = get_entry_per_seats_2022(df, columns, head=10)
+    seats_df = pd.DataFrame(df)
+    seats_df.set_index(df.index)[
+        ["entrée_moyenne"]].plot(kind='bar', figsize=(12, 6))
+    plt.table('Entrées moyenne par fauteuils dans les salles de cinémas')
+    plt.ylabel('entrée_moyenne')
+    plt.xlabel('commune')
+    plt.xticks(rotation=45)
+    plt.grid(axis='y')
+    plt.legend(title="Légende")
+    plt.show()
